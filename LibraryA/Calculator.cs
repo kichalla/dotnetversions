b68c5e64f40152a1922c0a61b33387c149018280
Kiran Challa <kichalla@microsoft.com>
2016-03-02 09:14:01 -0800

    Initial

---
+++ LibraryA/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LibraryA
+{
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ LibraryB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryB
+{
+    public class Class1
+    {
+        public string Greeting()
+        {
+            return "Hello World";
+        }
+    }
+}
